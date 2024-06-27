4dc0afe74aa7ec7c95b3f2b6565157c4e8a31d66
Issac Roy <issacroy05@gmail.com>
2024-06-27 16:16:49 -0700

    need images pls
    Co-authored-by: Edwin Abraham <EdwinKuttappi@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readdir("Images", (err, files) => {
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+  files.forEach(file => {
+    console.log(file);
+  });
+});
