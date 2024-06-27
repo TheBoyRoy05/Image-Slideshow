c3c37cf5cd9a3e19c504c6426ad5c0dc4f12d811
Issac Roy <issacroy05@gmail.com>
2024-06-27 16:59:53 -0700

    did some stuff
    Co-authored-by: Edwin Abraham <EdwinKuttappi@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+import { readdir } from 'fs';
 
-fs.readdir("Images", (err, files) => {
+readdir("./Images", (err, files) => {
   if (err) {
     return console.log('Unable to scan directory: ' + err);
   }
